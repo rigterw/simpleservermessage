f67e8a67c6bf48766525500b80239c8ee0d5f6d2
rigterw <rigterwessel@gmail.com>
2025-09-15 19:32:37 +0200

    project setup

---
+++ src/main/java/net/spoxy/App.java
@@ -0,0 +1,13 @@
+package net.spoxy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
